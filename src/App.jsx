6f19e96b1b5cb81ac5053823c03327943b9fe0bc
ivan-k-ivanow <i.ivanow.k@gmail.com>
2026-09-23 20:54:28 +0300

    Show add user modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './styles.css';
-
+import SaveUserModal from './components/SaveUserModal';
 import { useState, useEffect } from 'react';
 
 import Header from './components/Header';
@@ -9,38 +9,42 @@ import UserSearch from './components/UserSearch';
 import Pagination from './components/Pagination';
 
 function App() {
-  const [users, setUsers] = useState([]);
-  console.log(users);
+    const [users, setUsers] = useState([]);
+    const [showSaveUserModal, setShowSaveUserModal] = useState(false);
 
-  useEffect(() => {
-    fetch('https://wvxsshmemqotouxkzhqe.supabase.co/rest/v1/users', {
-      headers: {
-        'apikey': 'sb_publishable_YakXuX-44-KSSK3jvxe02Q__s_3zhC1',
-      }
-    })
-      .then(res => res.json())
-      .then(data => setUsers(data))
-      .catch(error => console.error('Error fetching users:', error));
-  }, []);
+    useEffect(() => {
+        fetch('https://wvxsshmemqotouxkzhqe.supabase.co/rest/v1/users', {
+            headers: {
+                'apikey': 'sb_publishable_YakXuX-44-KSSK3jvxe02Q__s_3zhC1',
+            }
+        })
+            .then(res => res.json())
+            .then(data => setUsers(data))
+            .catch(error => console.error('Error fetching users:', error));
+    }, []);
 
-  return (
-    <>
-      <Header />
+    const addUserClickHandler = () => {
+        setShowSaveUserModal(true);
+    }
 
-      <main className="main">
-        <section className="card users-container">
-          <UserSearch />
-          <UserList users={users} />
+    return (
+        <>
+            <Header />
 
-          <button className="btn-add btn">Add new user</button>s
+            <main className="main">
+                <section className="card users-container">
+                    <UserSearch />
+                    <UserList users={users} />
 
-          <Pagination />
-        </section>
-      </main>
+                    <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
+                    {showSaveUserModal && <SaveUserModal />}
+                    <Pagination />
+                </section>
+            </main>
 
-      <Footer />
-    </>
-  )
+            <Footer />
+        </>
+    )
 }
 
 export default App
