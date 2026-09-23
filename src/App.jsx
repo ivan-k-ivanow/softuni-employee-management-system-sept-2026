63a719fec01974faa6e4b67c9d204a37c38047f5
ivan-k-ivanow <i.ivanow.k@gmail.com>
2026-09-23 21:01:56 +0300

    close save user modal

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,10 @@ function App() {
         setShowSaveUserModal(true);
     }
 
+    const addUserCloseHandler = () => {
+        setShowSaveUserModal(false);
+    }
+
     return (
         <>
             <Header />
@@ -37,7 +41,7 @@ function App() {
                     <UserList users={users} />
 
                     <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
-                    {showSaveUserModal && <SaveUserModal />}
+                    {showSaveUserModal && <SaveUserModal onClose={addUserCloseHandler} />}
                     <Pagination />
                 </section>
             </main>
