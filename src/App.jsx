16456a7ae01dd58ea4a7789f3dff7ee419fee3ec
ivan-k-ivanow <i.ivanow.k@gmail.com>
2026-09-23 20:35:20 +0300

    Show dynamic users

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,14 @@ import Pagination from './components/Pagination';
 
 function App() {
   const [users, setUsers] = useState([]);
+  console.log(users);
 
   useEffect(() => {
-    fetch('https://wvxsshmemqotouxkzhqe.supabase.co/rest/v1/users')
+    fetch('https://wvxsshmemqotouxkzhqe.supabase.co/rest/v1/users', {
+      headers: {
+        'apikey': 'sb_publishable_YakXuX-44-KSSK3jvxe02Q__s_3zhC1',
+      }
+    })
       .then(res => res.json())
       .then(data => setUsers(data))
       .catch(error => console.error('Error fetching users:', error));
@@ -25,7 +30,7 @@ function App() {
       <main className="main">
         <section className="card users-container">
           <UserSearch />
-          <UserList />
+          <UserList users={users} />
 
           <button className="btn-add btn">Add new user</button>s
 
